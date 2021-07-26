d55bf6a070b81a09f02e03a939e153ef01039d1e
Kevin Martinez <mkevin755@gmail.com>
2021-07-25 21:13:13 -0600

    Add moment js library

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const moment = require("moment");
+
 const pepe = 2;
 const num = 3
 const sum = pepe + num;
 
+const myDate = moment().format("YYYY MM DD");
+
 console.log("Hello World", sum); 
+
+console.log("My Date =>", myDate);
